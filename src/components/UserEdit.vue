c2ccf075676e58cb53a0659bca9ce07339160bb7
jeo19 <terrakkk@outlook.com>
2021-10-14 10:19:50 +0800

    add:Add UserDetail Form UI components

--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -2,7 +2,13 @@
   <div class="yellow lighten-3 pa-3">
     <h3>You can change the user info</h3>
     <p>Change item</p>
-    <v-text-field label="Regular" v-model="user.name"></v-text-field>
+    <v-text-field label="Name" v-model="user.name"></v-text-field>
+    <v-text-field label="Address" v-model="user.address"></v-text-field>
+    <v-text-field label="Phone" v-model="user.phone"></v-text-field>
+    <v-radio-group v-model="user.hasDog">
+      <v-radio label="Has a dog" :value="true"></v-radio>
+      <v-radio label="Has not a dog" :value="false"></v-radio>
+    </v-radio-group>
   </div>
 </template>
 <script>
