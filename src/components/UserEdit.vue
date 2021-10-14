d48e081fbdd93bac427b15889d89f8dcb528cd25
jeo19 <terrakkk@outlook.com>
2021-10-14 10:08:45 +0800

    add:Add User Object in UserEdit component

--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -2,11 +2,20 @@
   <div class="yellow lighten-3 pa-3">
     <h3>You can change the user info</h3>
     <p>Change item</p>
-    <v-text-field label="Regular"></v-text-field>
+    <v-text-field label="Regular" v-model="user.name"></v-text-field>
   </div>
 </template>
 <script>
 export default {
   props: ["name", "address", "phone", "hasDog"],
+  data() {
+    return { user: {} };
+  },
+  created() {
+    this.user.name = this.name;
+    this.user.address = this.address;
+    this.user.phone = this.phone;
+    this.user.hasDog = this.hasDog;
+  },
 };
 </script>
